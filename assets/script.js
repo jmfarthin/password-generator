bffb845c5fe7ff4d5c4a4b942e9594326a08e405
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-02-07 16:25:22 -0500

    added prompt for number of characters and check to make sure is meets the criteria

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,24 @@
 // Assignment code here
+function generatePassword() {
+  let randomPassword = ""
 
+  let numCharacters = prompt("How many characters would you like your password to be? \nMinimum: 8 \nMaximum: 128");
+
+  numCharacters = parseInt(numCharacters)
+
+  if (!numCharacters) {
+    return;
+  }
+
+  if (typeof numCharacters === 'number' && numCharacters >= 8 && numCharacters <= 128) {
+    alert("Thank you!");
+  } else {
+    alert("You need at least 8 characters!");
+    return;
+  }
+
+  return numCharacters;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
